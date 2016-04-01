34af9e703c9fa1bc8929e1b0c96eb62c26332441
Joan Josep Jiménez <joanj_94@hotmail.com>
2016-04-01 10:09:39 +0200

    -> Afegit models a admin

--- applicationMarvel/admin.py
+++ applicationMarvel/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+import models
 
+admin.site.register(models.Character)
+admin.site.register(models.Comic)
+admin.site.register(models.Creator)
+admin.site.register(models.Event)
+admin.site.register(models.Story)
 # Register your models here.
