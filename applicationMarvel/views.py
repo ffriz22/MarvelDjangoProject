5d6b3b1b308356c9419ddbe002c8358bf9a69685
joanj94 <joanj_94@hotmail.com>
2016-04-04 12:42:28 +0200

    -> Afegida vista CreatorList/Detail -> Afegit template base i creator_list

--- applicationMarvel/views.py
+++ applicationMarvel/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.core import serializers
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import CreateView
+from models import Creator
 
+
+class ConnegResponseMixin(TemplateResponseMixin):
+
+    def render_json_object_response(self, objects, **kwargs):
+        json_data = serializers.serialize(u"json", objects, **kwargs)
+        return HttpResponse(json_data, content_type=u"application/json")
+
+    def render_xml_object_response(self, objects, **kwargs):
+        xml_data = serializers.serialize(u"xml", objects, **kwargs)
+        return HttpResponse(xml_data, content_type=u"application/xml")
+
+    def render_to_response(self, context, **kwargs):
+        if 'extension' in self.kwargs:
+            try:
+                objects = [self.object]
+            except AttributeError:
+                objects = self.object_list
+            if self.kwargs['extension'] == 'json':
+                return self.render_json_object_response(objects=objects)
+            elif self.kwargs['extension'] == 'xml':
+                return self.render_xml_object_response(objects=objects)
+        return super(ConnegResponseMixin, self).render_to_response(context)
+
+
+class CreatorList(ListView, ConnegResponseMixin):
+    model = Creator
+    queryset = Creator.objects
+    context_object_name = 'creator_list'
+    template_name = 'applicationMarvel/creator_list.html'
+#end of CreatorList class
+
+
+class CreatorDetail(DetailView, ConnegResponseMixin):
+    model = Creator
+    template_name = 'applicationMarvel/creator_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CreatorDetail, self).get_context_data(**kwargs)
+        return context
 # Create your views here.
