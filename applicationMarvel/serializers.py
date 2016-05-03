fc7c07f6091f8c77e1c7f74fa9754b4424e7bea6
Jaume Piñol <jaumepies@gmail.com>
2016-05-03 13:19:48 +0200

    -> Actualitzat requeriment.txt

---
+++ applicationMarvel/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework.fields import CharField
+from rest_framework.relations import HyperlinkedRelatedField, HyperlinkedIdentityField
+from rest_framework.serializers import HyperlinkedModelSerializer
+from models import
